7c8ab32a712c8cc457a241b70c509f2bc67c107e
jtb1137 <jtb1137@gmail.com>
2018-02-21 16:20:19 -0500

    typo fixes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
         @list = List.new(list_params)
 
         if @list.save
-            redirect_to list_page(@list)
+            redirect_to list_path(@list)
         else
             render 'new'
         end
@@ -34,7 +34,7 @@ class ListsController < ApplicationController
     private
 
     def list_params
-        params.require(list).permit(:name, :description)
+        params.require(:list).permit(:name, :description)
     end
 
     def set_list
