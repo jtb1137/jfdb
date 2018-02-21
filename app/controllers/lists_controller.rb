5156af228f0b4d4d0edb60dff42f3ccc9753b1e6
jtb1137 <jtb1137@gmail.com>
2018-02-21 16:14:51 -0500

    stub out list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,34 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def list_params
+        params.require(list).permit(:name, :description)
+    end
+
+    def set_list
+        @list = List.find(params[:id])
+    end
 end
