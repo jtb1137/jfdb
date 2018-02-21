94d6eca56a39619d0a853a9db040326944578553
jtb1137 <jtb1137@gmail.com>
2018-02-21 16:19:03 -0500

    fixed routing, can create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,15 +2,24 @@ class ListsController < ApplicationController
     before_action :set_list, only: [:show, :edit, :update, :destroy]
 
     def index
+        @lists = List.all
     end
 
     def show
     end
 
     def new
+        @list = List.new
     end
 
     def create
+        @list = List.new(list_params)
+
+        if @list.save
+            redirect_to list_page(@list)
+        else
+            render 'new'
+        end
     end
 
     def edit
