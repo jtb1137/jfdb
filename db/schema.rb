8e7ae2ca20dedfe2c89c771cf6fba84f5691b065
jtb1137 <jtb1137@gmail.com>
2018-02-21 16:12:32 -0500

    create list model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221200350) do
+ActiveRecord::Schema.define(version: 20180221210852) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
