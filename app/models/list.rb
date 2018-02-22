35d1049a71c7dc64de2804bc7910ab3dd5db60aa
jtb1137 <jtb1137@gmail.com>
2018-02-22 15:09:01 -0500

    added some validations and improved form layouts

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ApplicationRecord
-
+    validates :name, presence: true
+    validated :description, length: { maximum: 250 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+    validates :title, uniqueness: true
+    validates :title, presence: true
+    validates :synopsis, presence: true
+    validates :mpaa_rating, presence: true
 end
