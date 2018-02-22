600358a01afe2ca6c9428ab899d25cb3fadb9dfe
jtb1137 <jtb1137@gmail.com>
2018-02-22 13:34:47 -0500

    implemented some basic security on lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
     before_action :set_list, only: [:show, :edit, :update, :destroy]
+    before_action :security_check, only: [:edit, :update, :destroy]
 
     def index
         @lists = List.all
@@ -14,6 +15,7 @@ class ListsController < ApplicationController
 
     def create
         @list = List.new(list_params)
+        @list.user_id = current_user.id
 
         if @list.save
             redirect_to list_path(@list)
@@ -48,5 +50,11 @@ class ListsController < ApplicationController
 
     def set_list
         @list = List.find(params[:id])
+    end
+
+    def security_check
+        if current_user.id != @list.user_id
+            redirect_to root_path
+        end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222174655) do
+ActiveRecord::Schema.define(version: 20180222181549) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "movies", force: :cascade do |t|
